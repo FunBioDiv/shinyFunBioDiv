19fe3e0daf8318bafc42bbb965b3c836f7991dca
Romain Frelat <rom.frelat@gmail.com>
2026-06-22 15:14:40 +0200

    add Agrim, Agr4st, and A.Gardarin datasets and update PestiRed

--- analysis/03_update_data.R
+++ analysis/03_update_data.R
@@ -3,6 +3,10 @@
 # with update from Sylvie Ladet (Sebiopag_VcG) on 17/12/2026
 # with update from Frederic Fabre (OSCAR) on 03/04/2026
 # with Framework_BVD data from Axelle Tortosa on 05/05/2026
+# with Agrim data from Cendrine Mony on 31/05/2026
+# with data from Antoine Gardarin sent on 05/06/2026
+# with Agro4st data from Roberti Giotto on 18/06/2026
+# with PestiRed updates from Selma on 18/06/2026
 
 # Creates one files for the shiny app
 
@@ -67,8 +71,118 @@ frame <- frame[, names(meta)]
 # issue of duplicates, especially 236_b2, and 236_c2 with different coordinates
 # because only one year
 frame <- frame[!duplicated(frame[, 1:4]), ]
-
 meta <- rbind(meta, frame)
+
+
+# 5. add Agrim from Cendrine Mony on 31/05/2026
+agrim <- st_read("data/Parcelles_Agrim.gpkg")
+# select the centroid of the parcel
+agrim_pts <- st_centroid(agrim)
+# transform to lat/long
+agrim_pts <- st_transform(agrim_pts, "EPSG:4326")
+agrim_coo <- st_coordinates(agrim_pts)
+agrim_df <- data.frame(
+    "Study_ID" = "Agrim_2019",
+    "Plot_ID" = agrim_pts$AGRIM_2019,
+    "Year" = 2019,
+    "Crop_species" = agrim_pts$LIB_2019,
+    "Lat" = agrim_coo[, "Y"],
+    "Long" = agrim_coo[, "X"]
+)
+meta <- rbind(meta, agrim_df)
+
+# 6. add data from Antoine Gardarin on 05/06/2026
+seree <- read.csv2("data/Seree_2022/Data_pucerons.csv")
+# https://doi.org/10.1016/j.agee.2022.107902
+seree_df <- data.frame(
+    "Study_ID" = "Seree_2022",
+    "Plot_ID" = seree$Parcelle,
+    "Year" = seree$annee,
+    "Crop_species" = seree$culture,
+    "Lat" = seree$Latitude,
+    "Long" = seree$Longitude
+)
+seree_df <- seree_df[!duplicated(seree_df), ]
+meta <- rbind(meta, seree_df)
+
+pigot <- readxl::read_xlsx(
+    "data/Pigot_2023/Comptages GA 14.06.22 - JP - STATS.xlsx",
+    sheet = "GPS"
+)
+# https://theses.hal.science/tel-04453600v1
+pigot_df <- data.frame(
+    "Study_ID" = "Pigot_2023",
+    "Plot_ID" = paste(pigot$Parcelle, pigot$Quadrat, sep = "_"),
+    "Year" = substr(pigot$Campagne, 6, 9),
+    "Crop_species" = NA,
+    "Lat" = pigot$`Somme de Latitude N`,
+    "Long" = pigot$`Somme de Longitude E`
+)
+# table(duplicated(pigot_df))
+pigot_df <- pigot_df[!duplicated(pigot_df), ]
+meta <- rbind(meta, pigot_df)
+
+herrera <- readxl::read_xlsx(
+    "data/Herrera_2026/Correspondance codes parcelles.xlsx"
+)
+# https://doi.org/10.57745/HPEMYW
+herrera_df <- data.frame(
+    "Study_ID" = "Herrera_2026",
+    "Plot_ID" = paste(
+        herrera$Site_corrected,
+        herrera$`Code parcelle`,
+        herrera$ID_data_paper,
+        sep = "_"
+    ),
+    "Year" = herrera$Annee_corrected,
+    "Crop_species" = herrera$Especee_cultivee_corrected,
+    "Lat" = herrera$Latitude,
+    "Long" = herrera$Longitude
+)
+herrera_df <- herrera_df[!duplicated(herrera_df), ]
+meta <- rbind(meta, herrera_df)
+
+# 7. add Agro4st data from Roberti Giotto on 18/06/2026
+# https://www.agroforst.ch/daten-agro4esterie-2022/
+agro4st <- readxl::read_xlsx(
+    "data/Agro4st/Flaechen_Monitoring_Selma.xlsx"
+)
+coo <- strsplit(agro4st$`Coordinates (WGS)`, ", ")
+a22_df <- data.frame(
+    "Study_ID" = "Agro4st",
+    "Plot_ID" = paste(
+        agro4st$ID,
+        agro4st$Canton,
+        agro4st$Mode,
+        agro4st$Zone,
+        sep = "_"
+    ),
+    "Year" = 2022,
+    "Crop_species" = agro4st$`AF system`,
+    "Lat" = sapply(coo, function(x) as.numeric(x[1])),
+    "Long" = sapply(coo, function(x) as.numeric(x[2]))
+)
+a23_df <- a22_df
+a23_df$Year = 2023
+meta <- rbind(meta, a22_df, a23_df)
+
+# 8. add PestiRed data from Roberti Giotto on 18/06/2026
+pesti <- readxl::read_xlsx(
+    "data/PestiRed_all_siteData.xlsx"
+)
+pesti_df <- data.frame(
+    "Study_ID" = "PestiRed",
+    "Plot_ID" = pesti$ID_tech,
+    "Year" = pesti$year,
+    "Crop_species" = pesti$culture_code,
+    "Lat" = pesti$latitude,
+    "Long" = pesti$longitude
+)
+# table(duplicated(pesti_df)) # : 352
+# remove Pestired data in meta
+# table(meta$Study_ID == "PestiRed") # : 259
+meta <- rbind(meta[meta$Study_ID != "PestiRed", ], pesti_df)
+table(duplicated(meta)) # 2658
 
 # table(is.na(meta$Lat), meta$Study_ID, useNA = "ifany")
 write.csv(
